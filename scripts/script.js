0c5083a863e4bcc1a69354ecd37aada98e527273
Eric Tatchell <erictatch@gmail.com>
2022-11-02 16:29:11 -0700

    added stuff to food section of expenses

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,23 @@
 function sayHello() {
     
 }
-//sayHello();
+
+function foodSelect() {
+    localStorage.setItem('item', $('#').val())
+}
+function transSelect() {
+    document.getElementById('trans')
+}
+function clothesSelect() {
+    document.getElementById('clothes')
+}
+function techSelect() {
+    document.getElementById('tech')
+}
+function hotelSelect() {
+    document.getElementById('hotel')
+}
+function otherSelect() {
+    document.getElementById('other')
+}
+
