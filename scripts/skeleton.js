46cdae858fc6f7239bf3f1368a357fd257a6f310
Eric Tatchell Desktop <erictatch@gmail.com>
2022-11-02 21:15:36 -0700

    general cleanup and made fixed top nav

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -5,5 +5,6 @@
 function loadSkeleton(){
     console.log($('#navbarPlaceholder').load('./text/nav.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
+    console.log($('#navbarPlaceholderHome').load('./text/navHome.html'));
 }
 loadSkeleton();  //invoke the function
