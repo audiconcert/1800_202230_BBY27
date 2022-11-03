25e73e9e3501fdd62fe6408f9ad487f326ec1f00
Eric Tatchell <erictatch@gmail.com>
2022-11-03 12:19:17 -0700

    created firestore link

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,13 @@ function sayHello() {
     
 }
 
-function foodSelect() {
-    localStorage.setItem('item', $('#elementID').val())
-}
-function transSelect() {
-    document.getElementById('trans')
-}
-function clothesSelect() {
-    document.getElementById('clothes')
-}
-function techSelect() {
-    document.getElementById('tech')
-}
-function hotelSelect() {
-    document.getElementById('hotel')
-}
-function otherSelect() {
-    document.getElementById('other')
-}
+var source = document.getElementById('source').value
 
+var amount = document.getElementById('amount').value
+
+var dropdownFood = document.getElementById('food').value
+var dropdownClothes = document.getElementById('clothes').value
+var dropdownTech = document.getElementById('tech').value
+var dropdownHotel = document.getElementById('hotel').value
+var dropdownTransport = document.getElementById('trans').value
+var dropdownOther = document.getElementById('other').value
