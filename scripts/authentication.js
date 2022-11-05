656a148b56e6f8d958f3bf48084c520f0a01b72b
Eric Tatchell Desktop <erictatch@gmail.com>
2022-11-04 17:34:29 -0700

    added icon for all pages

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -19,9 +19,8 @@ var uiConfig = {
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
                     email: user.email,                         //with authenticated user's ID (user.uid)
-                    income: "0",                      //optional default profile info      
-                    expenses: "0",
-                    savingsGoals: "null"                     //optional default profile info
+                    country: "Canada",                      //optional default profile info      
+                    school: "BCIT"                          //optional default profile info
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
