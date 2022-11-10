d93f50a734ec0699ee38c189ba5330311ca08b67
Chelsey Joung <01CJ@protonmail.com>
2022-11-09 19:43:29 -0800

    added addSavings.js and made it to store name and amount.- CJ

---
+++ scripts/addSavings.js
@@ -0,0 +1,21 @@
+function addSavings() {
+    console.log("in");
+    let Name = document.getElementById("name").value;
+    let Amount = document.getElementById("savingsAmount").value;
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid);
+            var userID = user.uid;
+            console.log(userID);
+            db.collection("users").doc(user.uid).collection("savings").add({
+                name: Name,
+                userID: userID,
+                amount: Amount,
+            }).then(() => {
+                window.location.href = "savingsGoal.html"; //new line added
+            })
+        };
+    });
+}
+
