2726271cccbd2ca0b866336481b34ece3c9fe7c3
Eric Tatchell <erictatch@gmail.com>
2022-11-10 13:53:37 -0800

    favourite expenses now show on expense tab

---
+++ scripts/expense.js
@@ -0,0 +1,28 @@
+
+function showFavourite() {
+    let favouritestemplate = document.getElementById("favouritestemplate");
+    let favouriteCardGroup = document.getElementById("favouriteCardGroup");
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid);
+            var userID = user.uid;
+            console.log(userID);
+
+            currentUser.collection("favourites")
+                .limit(10)
+                .get()
+                .then(function (snap) {
+                    snap.forEach(function (doc) {       
+                        var amount = doc.data().amount;
+                        var source = doc.data().source;
+                        let testFavouriteCard = favouritestemplate.content.cloneNode(true);
+                        testFavouriteCard.querySelector('.card-title').innerHTML = amount;  
+                        testFavouriteCard.querySelector('.card-length').innerHTML = source; 
+                        favouriteCardGroup.appendChild(testFavouriteCard);
+                    })
+                })
+        }
+    })
+}
+showFavourite();
