af9f89f63ef91de38ba83d0e27c2f18e11e869cd
Chelsey Joung <01CJ@protonmail.com>
2022-11-10 18:15:46 -0800

    modified savingsGoals pages to function better and have cleaner layout -CJ

--- scripts/savingsGoal.js
+++ scripts/savingsGoal.js
@@ -1,6 +1,6 @@
 function showGoals() {
-    let favouritestemplate = document.getElementById("favouritestemplate");
-    let favouriteCardGroup = document.getElementById("favouriteCardGroup");
+    let favouritestemplate = document.getElementById("goalstemplate");
+    let favouriteCardGroup = document.getElementById("goalsCardGroup");
 
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -13,9 +13,11 @@ function showGoals() {
                 .then(function (snap) {
                     snap.forEach(function (doc) {
                         var goalAmount = doc.data().amount;
+                        var contributions = doc.data().contributions;
                         var name = doc.data().name;
                         let testFavouriteCard = favouritestemplate.content.cloneNode(true);
-                        testFavouriteCard.querySelector('.card-title').innerHTML = "$" + goalAmount;
+                        testFavouriteCard.querySelector('.card-title1').innerHTML = "$" + contributions;
+                        testFavouriteCard.querySelector('.card-title2').innerHTML = "/  $" + goalAmount;
                         testFavouriteCard.querySelector('.card-length').innerHTML = name;
                         favouriteCardGroup.appendChild(testFavouriteCard);
                     })
