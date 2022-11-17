e5f2a8712f878fb535b1d4a320b031bc3308a939
Chelsey Joung <01CJ@protonmail.com>
2022-11-17 00:19:20 -0800

    made edit button functional in profile.html. - CJ

--- scripts/account.js
+++ scripts/account.js
@@ -1,39 +1,51 @@
 var currentUser;
 
 function populateInfo() {
-    firebase.auth().onAuthStateChanged((user) => {
-        if(user) {
-            currentUser = db.collection("users").doc(user.uid);
-            currentUser.get().then((userDoc) => {
-                var userName = userDoc.data().name;
-                var userEmail = userDoc.data().email;
-                var treeName = userDoc.data().treeName;
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid);
+      currentUser.get().then((userDoc) => {
+        var userName = userDoc.data().name;
+        var userEmail = userDoc.data().email;
+        var treeNamE = userDoc.data().treeName;
 
-                if (userName != null) {
-                    document.getElementById("nameInput").value = userName;
-                }
-                if (userEmail != null) {
-                    document.getElementById("emailInput").value = userEmail;
-                }
-                if (treeName != null) {
-                    document.getElementById("treeNameInput").value = treeName;
-                }
-
-            });
-        } else {
-            console.log("No user is signed in");
+        if (userName != null) {
+          document.getElementById("nameInput").value = userName;
         }
-    });
+        if (userEmail != null) {
+          document.getElementById("emailInput").value = userEmail;
+        }
+        if (treeNamE != null) {
+          document.getElementById("treeNameInput").value = treeNamE;
+        }
+      });
+    } else {
+      console.log("No user is signed in");
+    }
+  });
 }
 
 populateInfo();
 
 function editUserInfo() {
-    document.getElementById("personalInfoFields").disabled = true;
+  document.getElementById("personalInfoFields").disabled = false;
 }
 
+// Can't save new (edited) info yet
 function saveUserInfo() {
-    userName = document.getElementById("nameInput").value;
-    userEmail = document.getElementById("emailInput").value;
-    treeName = document.getElementById("treeNameInput").value;
+  userName = form.document.getElementById("nameInput").value;
+  userEmail = form.document.getElementById("emailInput").value;
+  treeNamE = form.document.getElementById("treeNameInput").value;
+
+  currentUser
+    .update({
+      name: userName,
+      email: userEmail,
+      treeName: treeNamE,
+    })
+    .then(() => {
+      console.log("Document successfully updated");
+    });
+
+  document.getElementById("personalInfoFields").disabled = true;
 }
