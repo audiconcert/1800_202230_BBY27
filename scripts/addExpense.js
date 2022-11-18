aeb7fa102c5b4685643aebf4a18b9c3cc17fd01e
Chelsey Joung <01CJ@protonmail.com>
2022-11-17 19:52:56 -0800

    fixed addExpense.js and html to save expenses and favourites accordingly -CJ

--- scripts/addExpense.js
+++ scripts/addExpense.js
@@ -3,19 +3,20 @@ const source = document.getElementById('locale')
 const amount = document.getElementById('amt')
 const category = document.getElementById('category')
 const date = document.getElementById('date')
-// const EID = (""+Math.random()).substring(2,7);
 const form = document.getElementById('form')
 const save = document.getElementById('saveExp')
 const favourite = document.getElementById('addFav')
 
-// function addFav() {
-//     favourite.innerText = 'Added to Favourites!';
-// }
+function addFav() {
+    alert('Logged Expense and Added to Favourites!');
+    window.location.href = "expense.html";
 
-// function saveExp() {
-//     alert('Saved Expense!');
-//     window.location.href = "expense.html";
-// }
+}
+
+function saveExp() {
+    alert('Logged Expense!');
+    window.location.href = "expense.html";
+}
 
 
 const formatter = new Intl.NumberFormat('en-US', {
@@ -38,57 +39,25 @@ favourite.addEventListener('click', (e) => {
                 db.collection("users").doc(userID).set({
                     favourites: firebase.firestore.FieldValue.arrayUnion(expenseID)
                 }, {merge:true});
-            });
-
-            
+            });           
         }
-    })
-
-
+    });
+    setTimeout(addFav, 400);
 });
 
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    firebase.auth().onAuthStateChanged(user => {
+        if(user) {
+            var userID = user.uid;
+            console.log(userID);
+            db.collection("users").doc(user.uid).collection("expenses").add({
+                source: form.source.value,
+                category: form.category.value,
+                amount: parseFloat(form.amount.value),
+            });
+        }
+    });
+    setTimeout(saveExp, 400);
+});
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//             var userID = user.uid;
-//             console.log(userID);
-//             db.collection("users").doc(user.uid).collection("expenses").add({
-//                 // expenseID: EID, 
-//                 source: source.value,
-//                 category: form.category.value,
-//                 amount: parseFloat(amount.value),
-//                 // date: firebase.firestore.Timestamp.fromDate(date.valueAsDate = new Date())
-//             })
-            
-//             form.source.value = ''
-//             form.amount.value = ''
-//             form.date.value = ''
-//         };
-//     });
-//     setTimeout(saveExp, 400);
-// })
-
-
-// favourite.addEventListener('click', (e) => {
-//     if (form.amount.value > ''
-//         && form.source.value > ''
-//         && form.date.value > '') {
-//         e.preventDefault();
-//         firebase.auth().onAuthStateChanged(user => {
-//             if (user) {
-//                 var userID = user.uid;
-//                 console.log(userID);
-              
-//         })
-//     }
-//     setTimeout(addFav, 400);
-// })
-
-
-  // expenseID: EID,
-                    // source: form.source.value,
-                    // amount: parseFloat(amount.value),
-                    // category: form.category.value,
-                    // date: firebase.firestore.Timestamp.fromDate(date.valueAsDate = new Date())
