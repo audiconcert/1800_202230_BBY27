273a0b1dcbecc0c1c284ebf7127b71f5e61659fd
Eric Tatchell Desktop <erictatch@gmail.com>
2022-11-18 13:28:07 -0800

    chart skeleton

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -6,6 +6,23 @@ var imgCard = document.getElementById('imgCard');
 // var startDate = document.getElementById('startDate').value;
 // var endDate = document.getElementById('endDate').value;
 
-
-
+var ctx = document.getElementById('expenseChart').getContext('2d');
+var expenseChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Food & Beverage', 'Hotels, Clubs etc', 'Transportation', 'Clothes', 'Technology', 'Other', 'Subscriptions'],
+        datasets: [{
+            label: 'Category:',
+            data: [12, 19, 3, 5, 2, 3, 5],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
