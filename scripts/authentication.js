2af6c5544d4e2d742c120acca9387375f1e7de6b
Chelsey Joung <01CJ@protonmail.com>
2022-11-17 20:57:10 -0800

    added expenseCount to user's document and adding an expense from favourites will add that amount to expenseCount. -CJ

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -4,24 +4,15 @@ var uiConfig = {
     callbacks: {
       signInSuccessWithAuthResult: function (authResult, redirectUrl) {
         // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        //------------------------------------------------------------------------------------------
-        // The code below is modified from default snippet provided by the FB documentation.
-        //
-        // If the user is a "brand new" user, then create a new "user" in your own database.
-        // Assign this user with the name and email provided.
-        // Before this works, you must enable "Firestore" from the firebase console.
-        // The Firestore rules must allow the user to write. 
-        //------------------------------------------------------------------------------------------
-        var user = authResult.user;                            // get the user object from the Firebase authentication database
-        if (authResult.additionalUserInfo.isNewUser) {         //if new user
-            db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-                    name: user.displayName,                    //"users" collection
+        
+        var user = authResult.user;                            
+        if (authResult.additionalUserInfo.isNewUser) {         
+            db.collection("users").doc(user.uid).set({         
+                    name: user.displayName,                  
                     email: user.email, 
                     treeName: "Ernie",  
-                    favourites: firebase.firestore.FieldValue.arrayUnion(),                     //with authenticated user's ID (user.uid)
-                   //optional default profile info                   //optional default profile info
+                    favourites: firebase.firestore.FieldValue.arrayUnion(),  
+                    expenseCount: 0,                  
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
--- scripts/expense.js
+++ scripts/expense.js
@@ -14,97 +14,108 @@ function showFavourite() {
             var userID = user.uid;
             console.log(userID);
 
-            currentUser.collection("favourites")
-                .limit(50)
-                .orderBy("date")
-                .get()
-                .then(function (snap) {
+            currentUser.get().then(function(doc) {
+                var favourites = doc.data().favourites;
+                favourites.forEach(function(expenseIDs) {
+                    var expenseID = expenseIDs;
 
-                    snap.forEach(function (doc) {
+                    db.collection("users").doc(user.uid).collection("expenses").doc(expenseID)
+                    .get().then(function(doc) {
                         var amount = doc.data().amount;
                         var source = doc.data().source;
-                        // line 23 is new, go to expense.js to see more
-                        var expenseID = doc.id;
                         var category = doc.data().category;
-                        // var date = doc.data().date;
                         var testFavouriteCard = favouritestemplate.content.cloneNode(true);
                         testFavouriteCard.querySelector('.card-amount').innerHTML = formatter.format(parseFloat(amount));
                         testFavouriteCard.querySelector('.card-title').innerHTML = source;
                         testFavouriteCard.querySelector('.card-category').innerHTML = category;
-                        // testFavouriteCard.querySelector('.card-date').innerHTML = date;
 
+                        testFavouriteCard.querySelector('.card-amount').id = 'amount-' + expenseID;
+                        testFavouriteCard.querySelector('.card-title').id = 'title-' + expenseID;
+                        testFavouriteCard.querySelector('.card-category').id = 'category-' + expenseID;
+                        testFavouriteCard.querySelector('.add').id = 'add-' + expenseID;
+
+                        testFavouriteCard.querySelector('.add').onclick = () => addExistingFavourite(expenseID);
                         testFavouriteCard.querySelector('.edit').onclick = () => setExpenseData(expenseID);
                         testFavouriteCard.querySelector('.delete').onclick = () => deleteFavourite(expenseID);
-                        // from demo 11
-                        testFavouriteCard.querySelector('.card-title').id = 'source-' + expenseID;
-                        testFavouriteCard.querySelector('.card-amount').id = 'amount-' + expenseID;
-                        testFavouriteCard.querySelector('.card-category').id = 'category-' + expenseID;
-                        testFavouriteCard.querySelector('.card-date').id = 'date-' + expenseID;
-                        testFavouriteCard.querySelector('.add').id = 'add-' + expenseID;
-                        testFavouriteCard.querySelector('.add').onclick = () => addExistingFavourite(expenseID);
+
                         favouriteCardGroup.appendChild(testFavouriteCard);
-
-
-
-
                     });
                 });
+            });
         }
     });
 }
 showFavourite();
 
-// LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 function addExistingFavourite(expenseID) {
     firebase.auth().onAuthStateChanged(user => {
-        if (user) {
+        if(user) {
             var currentUser = db.collection("users").doc(user.uid);
-            currentUser.collection("favourites").where("expenseID", "==", expenseID)
-                .get()
-                .then(function (snap) {
-                    snap.forEach(function (doc) {
-                        currentUser.collection("expenses").add({
-                            amount: doc.data().amount,
-                            source: doc.data().source,
-                            expenseID: doc.data().expenseID,
-                            category: doc.data().category,
-                            date: doc.data().date
-                        })
-                    })  
-                })
-                .then(function () {
-                    var addID = 'add-' + expenseID;
-                    document.getElementById(addID).innerText = 'Added!';
-                })
+            currentUser.collection("expenses").doc(expenseID).get()
+            .then(function(doc) {
+                var Amount = doc.data().amount;
+                db.collection("users").doc(user.uid).get().then(function(doc) {
+                    var currentExpenseCount = doc.data().expenseCount;
+                    db.collection("users").doc(user.uid).set({
+                        expenseCount: currentExpenseCount + Amount,
+                    }, {merge:true});
+                });
+            });
         }
-    })
+    });
 }
 
-function setExpenseData(id) {
+// LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+// function addExistingFavourite(expenseID) {
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             var currentUser = db.collection("users").doc(user.uid);
+//             currentUser.collection("favourites").where("expenseID", "==", expenseID)
+//                 .get()
+//                 .then(function (snap) {
+//                     snap.forEach(function (doc) {
+//                         currentUser.collection("expenses").add({
+//                             amount: doc.data().amount,
+//                             source: doc.data().source,
+//                             expenseID: doc.data().expenseID,
+//                             category: doc.data().category,
+//                             date: doc.data().date
+//                         })
+//                     })  
+//                 })
+//                 .then(function () {
+//                     var addID = 'add-' + expenseID;
+//                     document.getElementById(addID).innerText = 'Added!';
+//                 })
+//         }
+//     })
+// }
 
-    var amountID = 'amount-' + id;
-    var amount = document.getElementById(amountID).innerHTML;
-    var sourceID = 'source-' + id;
-    var source = document.getElementById(sourceID).innerHTML;
-    var catID = 'category-' + id;
-    var category = document.getElementById(catID).innerHTML;
-    // var dateID = 'date-' + id;
-    // var date = document.getElementById(dateID).innerHTML;
-    localStorage.setItem('expenseID', id);
-    localStorage.setItem('expenseSource', source);
-    localStorage.setItem('expenseCategory', category);
-    localStorage.setItem('expenseAmount', amount);
-}
+// function setExpenseData(id) {
 
-// same issue as saving changes on an expense
-function deleteFavourite(id) {
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var favourite = db.collection("users").doc(user.uid).collection("favourites")
-                .where("expenseID", "==", id);
-            favourite.get().then(() => {
-                favourite.delete();
-            })
-        }
-    })
-}
+//     var amountID = 'amount-' + id;
+//     var amount = document.getElementById(amountID).innerHTML;
+//     var sourceID = 'source-' + id;
+//     var source = document.getElementById(sourceID).innerHTML;
+//     var catID = 'category-' + id;
+//     var category = document.getElementById(catID).innerHTML;
+//     // var dateID = 'date-' + id;
+//     // var date = document.getElementById(dateID).innerHTML;
+//     localStorage.setItem('expenseID', id);
+//     localStorage.setItem('expenseSource', source);
+//     localStorage.setItem('expenseCategory', category);
+//     localStorage.setItem('expenseAmount', amount);
+// }
+
+// // same issue as saving changes on an expense
+// function deleteFavourite(id) {
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             var favourite = db.collection("users").doc(user.uid).collection("favourites")
+//                 .where("expenseID", "==", id);
+//             favourite.get().then(() => {
+//                 favourite.delete();
+//             })
+//         }
+//     })
+// }
