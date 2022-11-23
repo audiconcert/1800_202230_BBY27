1d239465bc0ec75838f8fa194374aa4f79967981
Eric Tatchell Desktop <erictatch@gmail.com>
2022-11-22 20:31:07 -0800

    basic ernie function in analytics.js/html

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -3,52 +3,39 @@ var treeImg = document.getElementById('ernieImg');
 var imgTemplate = document.getElementById('imgTemplate');
 var imgCard = document.getElementById('imgCard');
 var now = new Date();
-now.setDate(now.getDate() + 14);
-
-// var startDate = document.getElementById('startDate').value;
-// var endDate = document.getElementById('endDate').value;
-
-var expenses;
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        var currentUser = db.collection("users").doc(user.uid);
-        var userID = user.uid;
-        console.log(userID);
-        currentUser.get().then(function (doc) {
-            var expenses = doc.data().expenseCount;
-        }); 
-    }
-});
-console.log(expenses);
-
-var ctx = document.getElementById('expenseChart').getContext('2d');
-var expenseChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Expenses', '>:('],
-        datasets: [{
-            label: 'Total:',
-            data: [expenses, 12],
-            borderWidth: 1
-        }]
-    }
-});
 
 
-var xValues = ['Expenses', 'Income'];
-var yValues = [expenses, 0];
-var barColors = ['red', 'green'];
-var cfc = document.getElementById('cashFlowChart').getContext('2d');
-var cashFlowChart = new Chart(cfc, {
-    type: 'bar',
-    data: {
-        labels: xValues,
-        datasets: [{
-            backgroundColor: barColors,
-            // label: 'Total:',
-            data: yValues,
-            borderWidth: 1,
-        }]
-    }
-});
+function insertTreeName() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then((userDoc) => {
+                var treeName = userDoc.data().treeName;
+                document.getElementById("treeName").innerText = treeName + "'s Health";
+            })
+        } else {
+            console.log('No user is signed in');
+        }
+    });
+}
+insertTreeName();
 
+function insertTree() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then((userDoc) => {
+                var expenses = userDoc.data().expenseCount;
+                var income = userDoc.data().incomeCount;
+                if (expenses > income) {
+                    var terrible = document.getElementById('tree');
+                    terrible.src = '/images/tree3.png'
+                } else if (expenses < income) {
+                    var terrible = document.getElementById('tree');
+                    terrible.src = '/images/tree1.png'
+                }
+            })
+        }
+    })
+}
+insertTree();
