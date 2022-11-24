fb9726b2e67f7e30cb2ed98fc427056daa5e51e0
Eric Tatchell Desktop <erictatch@gmail.com>
2022-11-23 19:17:24 -0800

    in its current state ernie is functional currently just tells you health percentage and finance stats huge strides

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,10 @@ function insertName() {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             currentUser = db.collection("users").doc(user.uid);
-            currentUser.get().then ((userDoc) => {
+            currentUser.get().then((userDoc) => {
                 var userName = userDoc.data().name;
                 var treeName = userDoc.data().treeName;
-                document.getElementById("name-goes-here").innerText = "Good to see you, " + userName;  
-                document.getElementById("tree-name-goes-here").innerText = treeName;
+                document.getElementById("name-goes-here").innerText = "Good to see you, " + userName;
             });
         } else {
             console.log('No user is signed in');
