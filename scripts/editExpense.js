4a7be9eb982be1409dd9b5c7c4d1b467b94c3794
Chelsey Joung <01CJ@protonmail.com>
2022-11-23 20:10:01 -0800

    changed edit.html and added editExpense.css. enabled users to edit their favourite  expense informations. -CJ

--- scripts/editExpense.js
+++ scripts/editExpense.js
@@ -1,66 +1,58 @@
-
+var form = document.getElementById("form");
 var sourceInput = document.getElementById("locale");
 var amountInput = document.getElementById("amt");
-// var categoryInput = document.getElementById("category");
-const expenseID = localStorage.getItem('id');
+var categoryInput = document.getElementById("category");
+const expenseID = localStorage.getItem("id");
 
-function populateInfo() {
-    firebase.auth().onAuthStateChanged((user) => {
-        if(user) {
-            var currentUser = db.collection("users").doc(user.uid);
-            currentUser.collection("expenses").doc(expenseID).get().then(function(doc) {
-                var Source = doc.data().source;
-                var Amount = doc.data().amount;
-                // var Category = doc.data().category;
+function populateExpenseInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      currentUser
+        .collection("expenses")
+        .doc(expenseID)
+        .get()
+        .then((doc) => {
+          var newSource = doc.data().source;
+          var newAmount = doc.data().amount;
+          var newCategory = doc.data().category;
 
-                if (Source != null) {
-                    sourceInput.value = Source;
-                }
-                if (Amount != null) {
-                    amountInput.value = Amount;
-                }
-                // if (Category != null) {
-                //     categoryInput.value = Category;
-                // }
-            });
-        } else {
-            console.log("No user signed in.");
-        }
-    });
+          if (newAmount != null) {
+            amountInput.value = newAmount;
+          }
+          if (newSource != null) {
+            sourceInput.value = newSource;
+          }
+          if (newCategory != null) {
+            categoryInput.value = newCategory;
+          }
+        });
+    } else {
+      console.log("No user is signed in.");
+    }
+  });
 }
-populateInfo();
+populateExpenseInfo();
 
-
-
-
-// function save() {
-//     alert("Saved!")
-//     window.location.href = "expense.html";
-// }
-
-// saveButton.addEventListener('click', (e) => {
-//     var expenseCategory = document.getElementById('category');
-//     var expenseSource = document.getElementById('locale');
-//     var expenseAmount = document.getElementById('amt');
-//     if (expenseCategory.value > ''
-//         && expenseSource.value > ''
-//         && expenseAmount.value > '') {
-//         e.preventDefault();
-//         firebase.auth().onAuthStateChanged(user => {
-//             if (user) {
-//                 var userID = user.uid;
-//                 console.log(userID);
-//                 var expense = db.collection("users").doc(userID).collection("expenses").where("expenseID", "==", expenseID);
-//                 expense.get().then(() => {
-//                     return expense.update({
-//                         category: expenseCategory,
-//                         source: expenseSource,
-//                         amount: parseFloat(expenseAmount),
-//                         date: firebase.firestore.Timestamp.fromDate((date.valueAsDate = new Date()))
-//                     })
-//                 })
-//             };
-//         })
-//     }
-//     setTimeout(save, 600);
-// });
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        var currentUser = db.collection('users').doc(user.uid);
+  
+        currentUser.collection('expenses').doc(expenseID).get().then((doc) => {
+          currentUser.collection('expenses').doc(expenseID).update({
+            amount: amountInput.value,
+            category: categoryInput.value,
+            source: sourceInput.value
+          });
+        });
+      }
+    });
+    setTimeout(save, 2000);
+  });
+  
+  function save() {
+    alert("Saved!")
+    window.location.href = "expense.html";
+  }
